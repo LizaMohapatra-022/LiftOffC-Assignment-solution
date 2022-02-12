aeddb463606c85239c64d344bff211e003c00dbf
LizaMohapatra-022 <99534731+LizaMohapatra-022@users.noreply.github.com>
2022-02-12 18:09:51 +0530

    Week 1 Solution

---
+++ W1_Q1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("NAME: LIZA MOHAPATRA \n");
+    printf("REGISTRATION NO: O22 \n");
+    printf("BRANCH: CIVIL \n");
+    printf("HOBBIES: READING, POETRY WRITING AND CYCLING\n");
+return 0;
+}
